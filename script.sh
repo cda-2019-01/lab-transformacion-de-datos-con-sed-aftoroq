fb0a1a95e2699f7c17a2d0f7cd9cdac81e6df221
Andrés Felipe Toro Quintero <atoroq@unal.edu.co>
2019-05-15 19:57:45 -0500

    Solución Laboratorio 01

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-# Escriba su código aquí
+﻿# Escriba su código aquí
+sed -e 's|\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)|20\3-\2-\1|' data.csv > data_2.csv
+sed -e 's|\([0-9]\)/\([0-9]\)/\([0-9][0-9][0-9][0-9]\)|\3-0\2-0\1|' data_2.csv > data_3.csv
+sed 's/;n/;\\n/g;s/;;/;\\n;/g;s/;N/;\\N/g' data_3.csv > data_4.csv
+sed -E 's/([[:lower:]])/\U\1/g' data_4.csv > data_5.csv
+sed 's/;[[:space:]]/; /g' < data_5.csv | sed 's/; /;\\N/g' > data_6.csv
+sed 's/,/./g' < data_6.csv | sed 's/;/,/g'  > solucion.csv
+rm data_*
+cat solucion.csv
