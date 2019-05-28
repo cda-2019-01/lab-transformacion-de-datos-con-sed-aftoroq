b3d5789dfc0ceb70ad47850bff24f12a93a64079
Andrés Felipe Toro Quintero <atoroq@unal.edu.co>
2019-05-27 22:33:32 -0500

    Solución Lab 01 - Grade corregido

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-﻿# Escriba su código aquí
+# Escriba su código aquí
 sed -e 's|\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)|20\3-\2-\1|' data.csv > data_2.csv
 sed -e 's|\([0-9]\)/\([0-9]\)/\([0-9][0-9][0-9][0-9]\)|\3-0\2-0\1|' data_2.csv > data_3.csv
 sed 's/;n/;\\n/g;s/;;/;\\n;/g;s/;N/;\\N/g' data_3.csv > data_4.csv
